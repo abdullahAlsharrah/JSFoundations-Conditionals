a60cd4cf72b1c5aa35b80d82e9d1d1c5d77c1cbf
Abdullah Alsharrah <abdullahalsharrah@Abdullahs-Air.hotspot>
2020-10-19 18:12:46 +0300

    Solved The Condotionals Task

--- calculator.js
+++ calculator.js
@@ -1,17 +1,18 @@
 // Define the numbers and the operation here
 
+let FirstNumber = 7;
+let SecondNumber = 4;
+let operation = "-";
+
 // Write if-statements here
 
-let FirstNumber = 7
-let SecondNumber = 4
-let operation = "+"
 
-if (opetaion = "+") {
-    console.log(FirstNumber + SecondNumber)
-} else if (Operation = "*") {
-    console.log(FirtsNumber * SecondNumber)
-} else if (operation = "/"){
-    console.log(FirtsNumber / SecondNumber)
-} else if (operation = "-") {
-    console.log(FirtsNumber - SecondNumber)
+if (operation === "+") {
+    console.log(`${FirstNumber} + ${SecondNumber} = ${FirstNumber + SecondNumber}`);
+} else if (operation === "*") {
+    console.log(`${FirstNumber} * ${SecondNumber} = ${FirstNumber * SecondNumber}`);
+} else if (operation === "/"){
+    console.log(`${FirstNumber} / ${SecondNumber} = ${FirstNumber / SecondNumber}`);
+} else if (operation === "-") {
+    console.log(`${FirstNumber} - ${SecondNumber} = ${FirstNumber - SecondNumber}`);
 }
