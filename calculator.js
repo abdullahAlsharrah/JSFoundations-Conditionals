8128b18d406e906a17dba30f81b5f4a51cf5d348
Abdullah Alsharrah <abdullahalsharrah@Abdullahs-Air.hotspot>
2020-10-19 17:20:24 +0300

    Solved the Conditionals Task

--- calculator.js
+++ calculator.js
@@ -1,3 +1,17 @@
 // Define the numbers and the operation here
 
 // Write if-statements here
+
+let FirstNumber = 7
+let SecondNumber = 4
+let operation = "+"
+
+if (opetaion = "+") {
+    console.log(FirstNumber + SecondNumber)
+} else if (Operation = "*") {
+    console.log(FirtsNumber * SecondNumber)
+} else if (operation = "/"){
+    console.log(FirtsNumber / SecondNumber)
+} else if (operation = "-") {
+    console.log(FirtsNumber - SecondNumber)
+}
